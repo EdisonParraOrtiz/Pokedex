cbe71fdbac19f11ee721fd98e7c8fcbd88156cb2
Edison Parra <edisonparraortiz8@gmail.com>
2022-09-03 18:57:28 -0500

    add pagination

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import Pagination from './Pagination'
 import PokemonCard from './Pokedex/PokemonCard'
 import SearchInput from './Pokedex/SearchInput'
 import SelectType from './Pokedex/SelectType'
@@ -9,66 +10,100 @@ import './styles/pokedex.css'
 
 const Pokedex = () => {
 
+  const nameTrainer = useSelector(state => state.nameTrainer)
+
   const [pokemons, setPokemons] = useState()
   const [pokeSearch, setPokeSearch] = useState()
   const [optionType, setOptionType] = useState('All')
+  const [currentBlock, setCurrentBlock] = useState(1)
+  const [page, setPage] = useState(0)
+
+
+
+
 
   useEffect(() => {
-    if(optionType !== 'All'){
-      // Aquí se hace la lógica de cuando el usuario quieres filtrar por tipo
+
+    if (optionType !== 'All') {
+      // aqi se hace la logica cuando el usuario qiere filtar por tipo
       const URL = `https://pokeapi.co/api/v2/type/${optionType}/`
       axios.get(URL)
         .then(res => {
           const arr = res.data.pokemon.map(e => e.pokemon)
-          setPokemons({results: arr})
+          setPokemons({ results: arr })
         })
         .catch(err => console.log(err))
-    } else if(pokeSearch){
-      // Aquí se hace la lógica cuando el usuario busca por el input
+    } else if (pokeSearch) {
+      // aqui se hace la logica cuando elusuario quiere buscar por el input
       const url = `https://pokeapi.co/api/v2/pokemon/${pokeSearch}`
-
       const obj = {
-        results: [{url}]
+        results: [{ url }]
       }
       setPokemons(obj)
     } else {
-      // Aquí se hace la lógica cuando el usuario quiere todos los pokemons
-      const URL = 'https://pokeapi.co/api/v2/pokemon'
+      // aqui se hace la logica cuando el usuario quiere todos los pokemons
+      const URL = `https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0`
       axios.get(URL)
         .then(res => setPokemons(res.data))
         .catch(err => console.log(err))
     }
-  }, [pokeSearch, optionType])
+  }, [pokeSearch, optionType, page])
 
-  const nameTrainer = useSelector(state => state.nameTrainer)
+
 
   return (
-    <div>
+    <div >
       <HeaderPoke />
-      <h2 className ='welcome'>Welcome {nameTrainer}, Catch them all.</h2>
-      <div className ='container-select'>
-      <SearchInput setPokeSearch={setPokeSearch} setOptionType={setOptionType} />
-      <div >
-       <SelectType  
-        optionType={optionType} 
-        setOptionType={setOptionType} 
-        setPokeSearch={setPokeSearch}
-       />
+      <h2 className ='welcome'>Welcome <span className ='welcome-span'>{nameTrainer}</span>, Catch them all.</h2>
+      <div className='select-pagination'>
+      <div className='container-select'>
+        <SearchInput
+          setPokeSearch={setPokeSearch}
+          setOptionType={setOptionType}
+        />
+        <SelectType
+          setPokeSearch={setPokeSearch} 
+          setOptionType={setOptionType} 
+          optionType={optionType} 
+          setPage={setPage}
+          setCurrentBlock={setCurrentBlock}
+        />
+       
       </div>
-    
-    </div>
+      <div className='container-pagination'>
+      <Pagination 
+          pokemons={pokemons}
+          page={page}
+          setPage={setPage}
+          currentBlock={currentBlock}
+          setCurrentBlock={setCurrentBlock}
+        />
+      </div>
+      </div>
+     
       <div className='cards-container'>
         {
-          pokemons?.results.map(pokemon => (
-            <PokemonCard 
+          pokemons?.results.slice(page * 10, (page + 1)*10).map(pokemon => (
+            <PokemonCard
               key={pokemon.url}
               url={pokemon.url}
             />
           ))
         }
       </div>
+      <footer>
+        <Pagination
+          pokemons={pokemons}
+          page={page}
+          setPage={setPage}
+          currentBlock={currentBlock}
+          setCurrentBlock={setCurrentBlock}
+        />
+      </footer>
     </div>
   )
+
+
 }
 
 export default Pokedex
